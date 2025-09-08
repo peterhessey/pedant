c608da760f6f5f337d556e73c7618b739b35118d
Peter Hessey <peter.hessey@zelim.com>
2025-09-08 13:13:43 +0100

    Use only pyright LSP

--- .config/nvim/lua/config/python.lua
+++ .config/nvim/lua/config/python.lua
@@ -1,12 +1,34 @@
--- Set python line length to 88 (ruff default + my fave)
-require("lspconfig").pylsp.setup({
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {
-          maxLineLength = 88,
-        },
-      },
-    },
-  },
-})
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pyright = {
+        settings = {
+          python = {
+            analysis = {
+              -- Keep these for functionality:
+              autoImportCompletions = true,
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              
+              -- Performance optimizations:
+              typeCheckingMode = "basic", -- vs "strict"
+              diagnosticMode = "openFilesOnly", -- vs "workspace" 
+              
+              -- Only exclude actual project junk:
+              exclude = {
+                "**/__pycache__",
+                "**/node_modules", 
+                "**/*.pyc",
+                ".git",
+                ".pytest_cache",
+                ".mypy_cache"
+              },
+              -- DON'T exclude .venv - pyright handles this automatically
+            }
+          }
+        }
+      }
+    }
+  }
+}
